777f13408dc4e5e530dadfef21914971018a6d8c
kevinkim <kevinkim.yhk@gmail.com>
2020-06-09 20:57:22 -0700

    create the fetch function to get the players from the backend

---
+++ src/player.js
@@ -0,0 +1,25 @@
+function getplayer(){
+document.getElementById("player_id").style.display="block"
+    fetch("http://localhost:3000/players") 
+    
+    .then(response => response.json()) 
+    .then(json => { 
+   
+        let li = ``; 
+        
+        json.forEach(player => { 
+            li += `<tr> 
+                <strong><td>Name:</strong> ${player.name} </td><br>
+                <strong><td>PPG:</strong> ${player.ppg}</td><br>
+                <strong><td>RPG:</strong> ${player.rpg}</td><br>
+                <strong><td>APG:</strong> ${player.apg}</td><br>
+                <strong><td>Description:</strong> ${player.description}</td><br>      
+            </tr><br>`; 
+            
+        });
+    
+    document.getElementById("player_id").innerHTML += li; 
+}); 
+};
+
+

0626297d7043b2bcec86e570fea447f7b0c8982d
kevinkim <kevinkim.yhk@gmail.com>
2020-06-09 21:14:19 -0700

    added in delete fetch request and an updated comment fetch request to reload the comments once a comment has been deleted

--- src/comment.js
+++ src/comment.js
@@ -9,6 +9,17 @@ function hide(){
 
 }
 
+function deleteComment(id){
+    fetch("http://localhost:3000/comments/" + id, {
+  method: 'DELETE',
+})
+.then(res => res.text())
+.then(res => {console.log(res) 
+
+displayUpdatedComment()
+})
+}
+
 function displayNewComment(){
     document.getElementById("content").style.display="block"
     fetch("http://localhost:3000/comments", {
@@ -31,4 +42,28 @@ function displayNewComment(){
     document.getElementById("content").innerHTML += li; 
     
 })
+}
+
+function displayUpdatedComment(){
+    fetch("http://localhost:3000/comments", {
+        method: 'GET',
+        
+})
+.then(resp => resp.json())
+.then(comment => {
+    console.log(comment)
+    let li = ``; 
+        comment.forEach(comment => { 
+            li += `<tr> 
+                <td>Name: ${comment.name} </td><br>
+                <td>Comment: ${comment.content}</td><br>  
+                <button type="button" onclick="deleteComment(${comment.id})">Delete</button><br>         
+            </tr><br>`; 
+            
+        }); 
+        
+    document.getElementById("content").innerHTML = li; 
+    
+})
+
 }

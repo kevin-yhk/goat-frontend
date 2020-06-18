9a45a9e5811c69b5fafe6d0d01eb5a0e09c36fc2
kevinkim <kevinkim.yhk@gmail.com>
2020-06-17 19:57:32 -0700

    refactored to OO

--- src/comment.js
+++ src/comment.js
@@ -1,41 +1,24 @@
+// function deleteComment(id){
+//     fetch("http://localhost:3000/comments/" + id, {
+//   method: 'DELETE',
+// })
+// .then(response => response.text())
+// .then(response => {console.log(response) 
 
-document.addEventListener("DOMContentLoaded", hide)
+// // displayUpdatedComment()
+// })
+// }
 
-
-function hide(){
-    displayNewComment()
-    document.getElementById("content").style.display="none"
-    document.getElementById("king").style.display="none"
-    document.getElementById("air").style.display="none"
-
-}
-
-function deleteComment(id){
-    fetch("http://localhost:3000/comments/" + id, {
-  method: 'DELETE',
-})
-.then(res => res.text())
-.then(res => {console.log(res) 
-
-displayUpdatedComment()
-})
-}
-
-function displayNewComment(){
-    document.getElementById("content").style.display="block"
-    fetch("http://localhost:3000/comments", {
-        method: 'GET',
-        
-    })
-.then(resp => resp.json())
-.then(comment => {
-    console.log(comment)
+class Comment{
+    static displayNewComment(){
+    API.getcomment()
+    .then(json => {
     let li = ``; 
-        comment.forEach(comment => { 
+        json.forEach(comment => { 
             li += `<tr> 
                 <td>Name: ${comment.name} </td><br>
                 <td>Comment: ${comment.content}</td><br>  
-                <button type="button" onclick="deleteComment(${comment.id})">Delete</button><br>         
+                <button type="button" onclick="Comment.deleteComment(${comment.id})">Delete</button><br>         
             </tr><br>`; 
             
         }); 
@@ -45,26 +28,54 @@ function displayNewComment(){
 })
 }
 
-function displayUpdatedComment(){
-    fetch("http://localhost:3000/comments", {
-        method: 'GET',
-        
-})
-.then(resp => resp.json())
-.then(comment => {
-    console.log(comment)
-    let li = ``; 
-        comment.forEach(comment => { 
-            li += `<tr> 
-                <td>Name: ${comment.name} </td><br>
-                <td>Comment: ${comment.content}</td><br>  
-                <button type="button" onclick="deleteComment(${comment.id})">Delete</button><br>         
-            </tr><br>`; 
+    static displayUpdatedComment(){
+        API.getcomment()
+        .then(json => {
+            let li = ``; 
+                json.forEach(comment => { 
+                    li += `<tr> 
+                        <td>Name: ${comment.name} </td><br>
+                        <td>Comment: ${comment.content}</td><br>  
+                        <button type="button" onclick="Comment.deleteComment(${comment.id})">Delete</button><br>         
+                    </tr><br>`; 
+                    
+                }); 
+                
+            document.getElementById("content").innerHTML += li; 
             
-        }); 
+        })
+
+    }
+
+    static deleteComment(id){
+        API.delete(id)
+        .then(response => {console.log(response)}) 
         
-    document.getElementById("content").innerHTML = li; 
-    
-})
+    }
+
 
 }
+
+// function displayUpdatedComment(){
+//     fetch("http://localhost:3000/comments", {
+//         method: 'GET',
+        
+// })
+// .then(resp => resp.json())
+// .then(comment => {
+//     console.log(comment)
+//     let li = ``; 
+//         comment.forEach(comment => { 
+//             li += `<tr> 
+//                 <td>Name: ${comment.name} </td><br>
+//                 <td>Comment: ${comment.content}</td><br>  
+//                 <button type="button" onclick="deleteComment(${comment.id})">Delete</button><br>         
+//             </tr><br>`; 
+            
+//         }); 
+        
+//     document.getElementById("content").innerHTML = li; 
+    
+// })
+
+// }

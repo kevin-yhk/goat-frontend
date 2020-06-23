458c7d7aa2151eb723809a7879fdbe08c14d05b4
kevinkim <kevinkim.yhk@gmail.com>
2020-06-22 19:10:44 -0700

    added in minor specifics to point out directed comments

--- src/comment.js
+++ src/comment.js
@@ -1,14 +1,3 @@
-// function deleteComment(id){
-//     fetch("http://localhost:3000/comments/" + id, {
-//   method: 'DELETE',
-// })
-// .then(response => response.text())
-// .then(response => {console.log(response) 
-
-// displayUpdatedComment()
-// })
-// }
-
 class Comment{
     static displayNewComment(){
     API.getcomment()
@@ -18,6 +7,7 @@ class Comment{
             li += `<tr> 
                 <td>Name: ${comment.name} </td><br>
                 <td>Comment: ${comment.content}</td><br>  
+                <td>Directed at: ${comment.player.name}</td><br>
                 <button type="button" onclick="Comment.deleteComment(${comment.id})">Delete</button><br>         
             </tr><br>`; 
             
@@ -36,6 +26,7 @@ class Comment{
                     li += `<tr> 
                         <td>Name: ${comment.name} </td><br>
                         <td>Comment: ${comment.content}</td><br>  
+                        <td>Directed at: ${comment.player.name}</td><br>
                         <button type="button" onclick="Comment.deleteComment(${comment.id})">Delete</button><br>         
                     </tr><br>`; 
                     
@@ -58,26 +49,3 @@ class Comment{
 
 }
 
-// function displayUpdatedComment(){
-//     fetch("http://localhost:3000/comments", {
-//         method: 'GET',
-        
-// })
-// .then(resp => resp.json())
-// .then(comment => {
-//     console.log(comment)
-//     let li = ``; 
-//         comment.forEach(comment => { 
-//             li += `<tr> 
-//                 <td>Name: ${comment.name} </td><br>
-//                 <td>Comment: ${comment.content}</td><br>  
-//                 <button type="button" onclick="deleteComment(${comment.id})">Delete</button><br>         
-//             </tr><br>`; 
-            
-//         }); 
-        
-//     document.getElementById("content").innerHTML = li; 
-    
-// })
-
-// }

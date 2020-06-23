653f84232f9320ff677d080429f6ce92b93f476f
kevinkim <kevinkim.yhk@gmail.com>
2020-06-22 19:08:51 -0700

    added in functions to post new comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 document.getElementById("whatever").addEventListener("click", showInfo)
-document.addEventListener("DOMContentLoaded", hide)
+document.addEventListener("DOMContentLoaded",hide)
+document.addEventListener("DOMContentLoaded", mountFormListener)
+
+const formName = document.querySelector("#comment_name")
+const formContent = document.querySelector("#comment_content")
+const formPlayerId = document.querySelector("#playerid")
+const postForm = document.getElementById("new_comment")
 
 
 function showInfo(){
@@ -18,4 +24,25 @@ function hide(){
     document.getElementById("king").style.display="none"
     document.getElementById("air").style.display="none"
     document.getElementById("new_comment").style.display="none"
+}
+
+function getCommentData(){
+    return {
+        name: formName.value,
+        content: formContent.value, 
+        player_id: parseInt(formPlayerId.value) 
+    }
+}
+
+function mountFormListener(){
+    postForm.addEventListener("submit", function(event){
+        event.preventDefault()
+
+        const commentObj = getCommentData()
+        if (postForm.dataset.action === "create"){
+            
+            API.create(commentObj)
+        }
+    })
+
 }
